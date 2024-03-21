2a65f6608ee4384594736af2ad6036ba426fc71a
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2024-03-21 11:09:27 -0300

    Initial commit

---
+++ app/src/main/java/br/senai/sp/jandira/MainActivity.kt
@@ -0,0 +1,179 @@
+package br.senai.sp.jandira
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.ui.theme.PlanetasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlanetasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Image(painter = painterResource(id = R.drawable.carros),
+        contentDescription = "Background da tela",
+        contentScale = ContentScale.Crop
+    )
+    Column(
+        modifier = Modifier
+            .background(Color(0x991A0A05))
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.,
+                        contentDescription = "Botão de busca",
+                        tint = Color.White
+                        )
+                }
+            },
+            placeholder = {
+                Text(
+                    text = "Buscar personagens",
+                    color = Color.White
+                    )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Color.White,
+                focusedBorderColor = Color(0xF6DDD5D5)
+            )
+
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "Lista de Planetas",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ){
+            items(10){
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(bottom = 12.dp),
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color(0xBBAFACAC)
+                    ),
+                    border = BorderStroke(
+                        width = 2.dp,
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(Color.Red, Color.Yellow)
+                        )
+                    )
+                ){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Card(
+                            modifier = Modifier
+                                .size(
+                                    width = 100.dp,
+                                    height = 100.dp
+                                ),
+                            shape = CircleShape
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.mcqueen),
+                                contentDescription = "",
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                        Column(
+                            modifier = Modifier
+                                .padding(start = 16.dp)
+                        ) {
+                            Text(
+                                text = "Carros 3",
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Yellow,
+                                letterSpacing = 4.sp
+                            )
+                            Text(
+                                text = "Durante uma corrida, Relâmpago McQueen acelerou demais e acabou perdendo o controle.",
+                                fontSize = 14.sp,
+                                color = Color.White,
+                                lineHeight = 20.sp
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    PlanetasTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting()
+        }
+    }
+}
